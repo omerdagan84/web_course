4986ea73defb08d5592b652bd2512f6052f727f6
omer.dagan <omerd@riscogroup.com>
2017-11-19 15:19:30 +0200

    site folder

---
+++ site/auxillary.js
@@ -0,0 +1,14 @@
+function display_ho_au() {
+	var box = document.getElementById("exthtml");
+	box.innerHTML = "<iframe src=\"home_aut.html\" seamless></iframe>"
+}
+
+function display_home() {
+	var box = document.getElementById("exthtml");
+	box.innerHTML = "<iframe src=\"home.html\" seamless></iframe>"
+}
+
+function display_web_des() {
+	var box = document.getElementById("exthtml");
+	box.innerHTML = "<iframe src=\"web_des.html\" seamless></iframe>"
+}
